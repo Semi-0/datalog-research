ba1134143bfd5ea19a1b395d7ba52dfa6fc21b20
PRim <longpp17@163.com>
2026-06-04 18:21:30 +0200

    added generic procedure propagators

--- run_tests.clj
+++ run_tests.clj
@@ -7,6 +7,7 @@
 (def all-test-namespaces
   '[bench-compare-test
     propagators-dispatch-test
+    propagators-generic-procedure-test
     propagators-network-test
     propagators-compound-data-test
     propagators-linked-list-access-test
