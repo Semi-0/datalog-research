da09ae8733efc2033a8c6fe232b97f75e8a29b84
PRim <longpp17@163.com>
2026-06-22 17:35:03 +0200

    Add accumulating GUR experiment

--- run_tests.clj
+++ run_tests.clj
@@ -22,6 +22,7 @@
     propagators.compound-object-test
     propagators.dispatch-test
     propagators.generic-procedure-test
+    propagators.gur-accumulating-test
     propagators.gur-subenv-test
     propagators.layered-procedure-test
     propagators.linked-list-access-test
